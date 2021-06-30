ecf21de21f2dfa4b8f3ffa764805556212e902b9
anjavojta1993 <anja.vojta@gmail.com>
2021-06-30 10:40:09 +0200

    Update api fetch url

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
     useCallback(async () => {
       // Call the API to retrieve the user information
       // by automatically passing along the sessionToken cookie
-      const response = await fetch('/api/');
+      const response = await fetch('/api/profile');
       const json = await response.json();
 
       // If there are errors, return early
