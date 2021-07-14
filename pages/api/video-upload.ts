97db00cf241d7dd0e25558a8ba02af7c4342dc41
anjavojta1993 <anja.vojta@gmail.com>
2021-07-14 21:22:11 +0200

    Add test api route

---
+++ pages/api/video-upload.ts
@@ -0,0 +1,43 @@
+import { v2 as cloudinary } from 'cloudinary';
+import Formidable from 'formidable';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const endpoint = async (req: NextApiRequest, res: NextApiResponse) => {
+	cloudinary.config({
+		cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME,
+		api_key: process.env.CLOUDINARY_API_KEY,
+		api_secret: process.env.CLOUDINARY_API_SECRET,
+	});
+
+	const data = await new Promise<{ file: Formidable.File }>(
+		(resolve, reject) => {
+			const form = new Formidable.IncomingForm({
+				keepExtensions: true,
+				multiples: false,
+			});
+			form.parse(req, (err, fields, files) => {
+				if (err) return reject(err);
+				resolve({ file: files.file as Formidable.File });
+			});
+		}
+	);
+
+	try {
+		const uploadedVideo = await cloudinary.uploader.upload(data.file.path);
+		res.statusCode = 200;
+		res.json({ videoUrl: uploadedVideo.secure_url });
+	} catch (error) {
+		console.log(error);
+		res.statusCode = 500;
+		res.json({ videoUrl: 'error' });
+	}
+};
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+export default endpoint;
