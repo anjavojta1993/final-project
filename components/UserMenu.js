95c567b2ded018baa808d6265654d306ae21e728
anjavojta1993 <anja.vojta@gmail.com>
2021-07-16 15:23:00 +0200

    Finish user menu

--- components/UserMenu.js
+++ components/UserMenu.js
@@ -1,17 +1,67 @@
 import { css } from '@emotion/react';
+import Link from 'next/link';
 import { normalText } from '../styles/sharedStyles';
 
 const navBarContainer = css`
   margin: 0;
   display: flex;
-  height: 100%;
+  height: 100vh;
+  width: 200px;
   align-self: flex-start;
-  flex-direction: row;
-  justify-content: center;
-  background-color: orange;
+  flex-direction: column;
+  //background-color: orange;
   border-right: 1px solid black;
+
+  p {
+    display: flex;
+    padding-top: 10px;
+    justify-content: center;
+    font-size: ${normalText};
+  }
+
+  a {
+    margin-top: 20%;
+    display: flex;
+    justify-content: center;
+    font-size: ${normalText};
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    text-decoration: none;
+    color: black;
+    font-weight: 500;
+  }
 `;
 
-export default function UserMenu() {
-  return <div css={navBarContainer}>TEST</div>;
+const profileImageContainer = css`
+  display: flex;
+  margin-top: 20%;
+  //background-color: blue;
+  height: 150px;
+
+  > img {
+    width: auto;
+    height: auto;
+  }
+`;
+
+export default function UserMenu(props) {
+  return (
+    <div css={navBarContainer}>
+      <div css={profileImageContainer}>
+        <img
+          src="/images/avatar.png"
+          alt="woman with dark skin and afro and happy face avatar"
+        />
+      </div>
+      <p>
+        Welcome, {props.firstName} {props.lastName}
+      </p>
+      <Link href={`/profiles/${props.userId}`}>
+        <a>Profile</a>
+      </Link>
+      <Link href="/logout">
+        <a>Logout</a>
+      </Link>
+    </div>
+  );
 }
