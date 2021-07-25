bbfa021d5b5198b3d294d7a4ae5ea67b016786a6
anjavojta1993 <anja.vojta@gmail.com>
2021-07-25 22:38:21 +0200

    Update font size

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,8 +1,11 @@
+import { css } from '@emotion/react';
 import cookie from 'cookie';
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import { useEffect } from 'react';
+import { FaHeart } from 'react-icons/fa';
 import Layout from '../components/Layout';
+import { largeText } from '../styles/sharedStyles';
 import { deleteSessionByToken } from '../util/database';
 
 type Props = {
@@ -10,6 +13,20 @@ type Props = {
   email: string;
   userId: number;
 };
+
+const pageContainer = css`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+const logoutContainer = css`
+  justify-content: center;
+  font-size: ${largeText};
+  font-weight: 200;
+`;
 
 export default function Logout(props: Props) {
   useEffect(() => props.refreshEmail(), [props]);
@@ -19,8 +36,12 @@ export default function Logout(props: Props) {
       <Head>
         <title>Logout</title>
       </Head>
-
-      <div data-cy="logout-page-h1">You have been logged out successfully!</div>
+      <div css={pageContainer}>
+        <div css={logoutContainer} data-cy="logout-page-h1">
+          You have been logged out successfully. Please come back soon.{' '}
+          <FaHeart />
+        </div>
+      </div>
     </Layout>
   );
 }
