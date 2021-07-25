eff6605db7ebe15a02a21935dc4268fe7e3f485f
anjavojta1993 <anja.vojta@gmail.com>
2021-07-25 22:38:40 +0200

    Update font size

--- pages/profiles/creation-successful.tsx
+++ pages/profiles/creation-successful.tsx
@@ -1,10 +1,29 @@
+import { css } from '@emotion/react';
 import Head from 'next/head';
 import Layout from '../../components/Layout';
+import { largeText } from '../../styles/sharedStyles';
 
 type Props = {
   email: string;
   userId: number;
 };
+
+const pageContainer = css`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+const loginContainer = css`
+  justify-content: center;
+
+  h2 {
+    font-size: ${largeText};
+    font-weight: 200;
+  }
+`;
 
 export default function SuccessfulCreation(props: Props) {
   return (
@@ -12,7 +31,11 @@ export default function SuccessfulCreation(props: Props) {
       <Head>
         <title>Login</title>
       </Head>
-      <h1>Thank you, your profile page has been successfully created.</h1>
+      <div css={pageContainer}>
+        <div css={loginContainer}>
+          <h2>Thank you, your profile page has been successfully created.</h2>
+        </div>
+      </div>
     </Layout>
   );
 }
